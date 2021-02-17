c358d0fe3d7a3849d0e03f409de40a5d00d789d2
Genne23v <59734889+Genne23v@users.noreply.github.com>
2021-02-16 21:46:59 -0500

    lab complete

--- WS05/lab/Account.cpp
+++ WS05/lab/Account.cpp
@@ -1,3 +1,9 @@
+/**********************************************************
+* Name: Wonkeun No
+* Student ID: 145095196
+* Seneca email: wno@myseneca.ca
+* Section: NGG
+**********************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "Account.h"
@@ -20,6 +26,101 @@ namespace sdds {
          m_number = number;
          m_balance = balance;
       }
+   }
+   Account& Account::operator=(int account)
+   {
+       if (!(bool)account)
+       {
+           setEmpty();
+       }
+       if (~account == true)
+       {
+           this->m_number = account;
+       }
+       return *this;
+   }
+   Account& Account::operator=(Account& account)
+   {
+       if (((bool)*this || ~*this) && ((bool)account || ~account))
+       {
+           this->m_number = account.m_number;
+           this->m_balance = account.m_balance;
+           account.m_number = 0;
+           account.m_balance = 0.0;
+       }
+       return *this;
+   }
+   Account& Account::operator+=(double depositMoney)
+   {
+       if ((bool)*this && depositMoney >= 0.0)
+       {
+           this->m_balance += depositMoney;
+       }
+       return *this;
+   }
+   Account& Account::operator-=(double withdrawMoney)
+   {
+       if ((bool)*this && withdrawMoney >= 0.0 && this->m_balance > withdrawMoney)
+       {
+           this->m_balance -= withdrawMoney;
+       }
+       return *this;
+   }
+   Account& Account::operator<<(Account& account)
+   {
+       if ((bool)*this && (bool)account && this != &account)
+       {
+           this->m_balance += account.m_balance;
+           account.m_balance = 0.0;
+       }
+       return *this;
+   }
+   Account& Account::operator>>(Account& account)
+   {
+       if ((bool)*this && (bool)account)
+       {
+           account.m_balance += this->m_balance;
+           this->m_balance = 0.0;
+       }
+       return *this;
+   }
+   Account::operator bool() const
+   {
+       return (m_number >= 10000 && m_number <= 99999) && (m_balance >= 0);
+   }
+   Account::operator int() const 
+   {
+       return m_number;
+   }
+   Account::operator double() const
+   {
+       return m_balance;
+   }
+   bool operator~(const Account& account)
+   {
+       return ((int)account == 0);
+   }
+   double operator+(const Account& account, const Account& other)
+   {
+       double sum = 0.0;
+       if ((bool)account && (bool)other)
+       {
+           sum = (double)account + (double)other;
+       }
+       else
+       {
+           sum = 0.0;
+       }
+       return sum;
+   }
+   double& operator+=(double& balance, const Account& account)
+   {
+       double sum;
+       if ((bool)account)
+       {
+           sum = balance + (double)account;
+       }
+       return sum;
    }
    std::ostream& Account::display() const{
       if (*this) {  
--- WS05/lab/Account.h
+++ WS05/lab/Account.h
@@ -1,21 +1,35 @@
+/**********************************************************
+* Name: Wonkeun No
+* Student ID: 145095196
+* Seneca email: wno@myseneca.ca
+* Section: NGG
+**********************************************************/
 #ifndef SDDS_ACCOUNT_H_
 #define SDDS_ACCOUNT_H_
 #include <iostream>
 
 namespace sdds {
-   class Account {
-      int m_number;
-      double m_balance; 
-      void setEmpty();
-   public:
-      Account();
-      Account(int number, double balance);
-      std::ostream& display()const;
-
-
-
-   };
-   
-   
+	class Account {
+		int m_number;
+		double m_balance;
+		void setEmpty();
+	public:
+		Account();
+		Account(int number, double balance);
+		std::ostream& display()const;
+		operator bool() const; 
+		operator int() const;
+		operator double() const;
+		
+		Account& operator=(int account);
+		Account& operator=(Account& account);
+		Account& operator+=(double depositMoney);
+		Account& operator-=(double withdrawMoney);
+		Account& operator<<(Account& account);
+		Account& operator>>(Account& account);
+	};
+	bool operator~(const Account& account);
+	double operator+(const Account& account, const Account& other);
+	double& operator+=(double& balance, const Account& account);
 }
 #endif // SDDS_ACCOUNT_H_
