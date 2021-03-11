e2df158b6dd6e6e317483f260ddc59928d026f6f
Genne23v <59734889+Genne23v@users.noreply.github.com>
2021-03-11 00:56:35 -0500

    line break issue

--- WS06/lab/TextFile.cpp
+++ WS06/lab/TextFile.cpp
@@ -190,7 +190,7 @@ namespace sdds {
 				ostr << m_textLines[i].m_value << endl;
 				if ((i + 1) % m_pageSize == 0)
 				{
-					ostr << "Hit ENTER to continue...";
+					ostr << "Hit ENTER to continue..." << endl;
 					cin.ignore(1000, '\n');
 				}
 			}
